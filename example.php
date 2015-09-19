e2066ecd49dd7df0cb05a42edda1ef9bc423659d
Turan Karatuğ <tkaratug@hotmail.com.tr>
2015-09-20 02:21:14 +0300

    Update example.php

--- example.php
+++ example.php
@@ -4,7 +4,7 @@ include 'currency.php';
 
 $kur = new TCMB_currency(10); // 10 dakika önbellekte tutulur
 
-echo 'Default (BanknoteBuying) : ' . $kur->convert('TRY','USD',25) . '<br>';
+echo 'Default (ForexBuying) : ' . $kur->convert('TRY','USD',25) . '<br>';
 echo 'BanknoteBuying : ' . $kur->convert('TRY','USD',25, 'BanknoteBuying') . '<br>';
 echo 'BanknoteSelling : ' . $kur->convert('TRY','USD',25, 'BanknoteSelling') . '<br>';
 echo 'ForexBuying : ' . $kur->convert('TRY','USD',25, 'ForexBuying') . '<br>';
